9811324656f48d6b34a376a734a6e78a481d427c
thetemka <thetemka@gmail.com>
2018-03-11 15:34:58 +0300

    parser add

--- ta_main.py
+++ ta_main.py
@@ -1,14 +1,25 @@
 import ta_authorize
 import ta_actions
+import argparse
+import sys
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--check_box")
+parser.add_argument("--email")
+parser.add_argument("--password")
+options = parser.parse_args()
+
+#print(args)
 #print('Введите емейл:')
 #email = input()
 #print('Введите пароль:')
 #password = input()
 
-email = 'test.box706@mail.ru'
-password = '003Solver'
-
+#email = 'test.box706@mail.ru'
+#password = '003Solver'
+email = options.email
+state = options.check_box
+password = options.password
 ta_authorize.authorize(email, password)
 ta_actions.actions(email,password)
 ta_actions.check_box(state)
